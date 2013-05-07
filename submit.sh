a9e8d1607b28e570b7037ab961c6d55f23bd86a6
vonnyfly <lifeng1519@gmail.com>
2013-05-07 21:33:45 +0800

    remove objs

--- submit.sh
+++ submit.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -o nounset          # Treat unset variables as an error
-
-rsync -avu --exclude=submit.sh ~/webcam/ pi@192.168.1.107:/home/pi/webcam/  
-#ssh pi@192.168.1.107 "cd ~/webcam;python webiopi_custom.py"
-#ssh pi@192.168.1.107 "ps aux | grep -i 'python webiopi_custom.py' | head -n 1 | awk '{print $2}' | xargs kill -9"
-
+git add .
+git commit -a -m "$1"
+git push origin master
 
